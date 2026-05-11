7703c240c154a8837d89ef09d2756cbe4232cf15
riocraftma-mtrnl <riocraftma@gmail.com>
2026-05-11 16:10:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("from script file");
+const cards = document.querySelectorAll(
+  ".project-card"
+);
+
+cards.forEach((card) => {
+
+  card.addEventListener("mousemove", (e) => {
+
+    const rect =
+      card.getBoundingClientRect();
+
+    const x =
+      e.clientX - rect.left;
+
+    const y =
+      e.clientY - rect.top;
+
+    card.style.setProperty(
+      "--x",
+      `${x}px`
+    );
+
+    card.style.setProperty(
+      "--y",
+      `${y}px`
+    );
+
+  });
+
+});
